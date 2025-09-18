b3e92a246a9da313105a52f557ed7819256f2b02
Kleber Santos <kleberabap@gmail.com>
2025-09-18 18:11:13 -0300

    Feat: Project Cleanup

--- desafio04_python/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Diretório onde ficam os dados, caso a variável não esteja setada, usa "./data"    
-DATA_DIR = os.environ.get("VR_DATA_DIR", "./data")
-
-# Arquivos de saída
-RESULT_FILE = os.path.join(DATA_DIR, "VR_MENSAL_05_2025_RESULTADO.xlsx")
-VALIDACAO_FILE = os.path.join(DATA_DIR, "VR_MENSAL_05_2025_VALIDACAO.xlsx")
